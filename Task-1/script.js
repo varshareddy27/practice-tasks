83f67b55ae7559a05833957c81ed44b185a6e4a7
varshareddy27 <127618823+varshareddy27@users.noreply.github.com>
2024-02-18 23:56:43 +0530

    script.js

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,2 +1,8 @@
-let c=document.getElementById("abc")
-c.style.color="red"
+document.getElementsByTagName("nav")[0].firstElementChild.style.color="red"
+//3)
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
+//4)
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+  element.style.background = "cyan";
+})     
